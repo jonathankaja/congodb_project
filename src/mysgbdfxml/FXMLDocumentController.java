7e91560880a228b7cea49c0d1e9b9d75f0d0c585
Madi Z Patrick <16mz214@esisalama.org>
2019-06-19 21:34:40 -0700

    ajout et modification de quelque methode de transite (suite)

--- src/mysgbdfxml/FXMLDocumentController.java
+++ src/mysgbdfxml/FXMLDocumentController.java
@@ -6,11 +6,14 @@
 
 package mysgbdfxml;
 
-import java.awt.event.MouseEvent;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 
 /**  
@@ -33,13 +36,82 @@ public class FXMLDocumentController implements Initializable {
     
     @FXML Pane loginPane = new Pane();
     @FXML Pane homePane = new Pane();
+    @FXML Pane mesbd = new Pane();
+    @FXML Pane add_bd = new Pane();
+    @FXML ImageView deconnexion=new ImageView();
+    @FXML TextField user = new TextField();
+    @FXML PasswordField pwd =new PasswordField();
+    @FXML Button mesbd_retour =new Button();
+    @FXML Button retour_in_add_bd =new Button();
+    @FXML Button creer_in_add_bd =new Button();
     
+    
+    /**
+     *
+     * @throws Exception
+     */
     @FXML
     public void connexion() throws Exception 
     {
-       homePane.setVisible(true);
-       loginPane.setVisible(false);
+        String log = user.getText();
+        String mdp = pwd.getText();
+        if("a".equals(log) && "a".equals(mdp)){
+            homePane.setVisible(true);
+            loginPane.setVisible(false);
+        }
+        else{
+            System.out.println("mysgbdfxml.FXMLDocumentController.deconnexion()");
+        }
+       
     }
+    @FXML
+    public void deconnexion() throws Exception 
+    {
+        homePane.setVisible(false);
+        loginPane.setVisible(true);
+       
+    }
+    @FXML
+    public void mesbd_retour() throws Exception 
+    {
+        homePane.setVisible(true);
+        mesbd.setVisible(false);
+       
+    }
+    @FXML
+    public void mesbd() throws Exception 
+    {
+        homePane.setVisible(false);
+        mesbd.setVisible(true);
+       
+    }
+    
+    @FXML
+    public void new_bd() throws Exception 
+    {
+        homePane.setVisible(false);
+        add_bd.setVisible(true);
+       
+    }
+    
+    @FXML
+    public void retour_in_add_bd() throws Exception 
+    {
+        add_bd.setVisible(false);
+        homePane.setVisible(true);
+        
+       
+    }
+    @FXML
+    public void creer_in_add_bd() throws Exception 
+    {
+        System.out.println("BD creer avec success !");
+       
+    }
+    
+    
+    
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         //
